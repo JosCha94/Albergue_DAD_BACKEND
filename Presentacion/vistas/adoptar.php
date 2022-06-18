3c1e563bec99fbe7bc70b56c5d6994bba0040f11
FabricioRMK3 <fabriciovc16@gmail.com>
2022-06-18 10:14:39 -0500

    adopciones

--- Presentacion/vistas/adoptar.php
+++ Presentacion/vistas/adoptar.php
@@ -1,1 +1,53 @@
-<h1 class="text-center"> VISTA DE ADOPCIONES</h1>
+
+<div class="table-responsive my-5">
+    <table id="admin-table" class="table table-striped">
+        <thead class="table-heading bg-info">
+            <tr>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+                <th scope="col">Position</th>
+                <th scope="col">Office</th>
+                <th scope="col">Age</th>
+                <th scope="col">Start date</th>
+                <th scope="col">Salary</th>
+                <th scope="col">Extn.</th>
+                <th scope="col">E-mail</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger</td>
+                <td>Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+                <td>5421</td>
+                <td>t.nixon@datatables.net</td>
+            </tr>
+            <tr>
+                <td>Tiger</td>
+                <td>Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+                <td>5421</td>
+                <td>t.nixon@datatables.net</td>
+            </tr>
+            <tr>
+                <td>Tiger</td>
+                <td>Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+                <td>5421</td>
+                <td>t.nixon@datatables.net</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
