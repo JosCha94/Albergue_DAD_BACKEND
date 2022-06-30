1ef184f639f47eab87ad6454e4b824b1e0f13cd9
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2022-06-30 12:35:42 -0500

    yes

--- index.php
+++ index.php
@@ -340,5 +340,4 @@ switch ($error = 'SinError') {
 <script src="Presentacion/libs/javascript/script.js"></script>
 <script src="Presentacion\libs\datatable\jquery.dataTables.min.js"></script>
 <script src="Presentacion\libs\datatable\dataTables.bootstrap5.min.js"></script>
-
 </html>
